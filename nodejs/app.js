854f8170ce888cd1e966bc96123b5e72bd8c9369
Nicolas Donoso <nicolas@azlo.com>
2020-02-10 21:52:21 -0800

    add more sutff

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,14 +1,38 @@
 const http = require('http');
+const fs = require('fs');
 // const https = require('https');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<html>');
+        return res.end();
+    }
+    if (url === '/message'&& method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+        // fs.writeFileSync('message.txt', 'DUMMY');
+        res.writeHead(302, { 'Location':'/' });
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first page</title></head>');
     res.write('<body><h1>LAlaLALLAL</hi></body>');
-    res.write('/<html>');
+    res.write('<html>');
     res.end();
 });
 
